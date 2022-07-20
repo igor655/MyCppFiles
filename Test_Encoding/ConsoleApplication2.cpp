36e176a67266be20d6d43910cda52c7e9121d17a
igor655 <belyyigor@gmail.com>
2022-07-20 14:02:40 +0300

    Test encoding
    add folder Test_Encoding
    	new file:   Test_Encoding/ConsoleApplication2.cpp

---
+++ Test_Encoding/ConsoleApplication2.cpp
@@ -0,0 +1,24 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{
+    //setlocale(LC_ALL, "ru");
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    std::cout << "ророррлорлорл\n";
+    /*char ch;
+    cin >> ch;
+    cout << ch << endl;*/
+    string str;
+    cin >> str;
+    cout << str << endl;
+}
+
